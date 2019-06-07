541b7f94cc54ec28ca24fd9dea5ef22e5e73b291
magdanat <nathanmagdalera@gmail.com>
2019-06-07 12:40:51 -0700

    Work on authentication and validating fields

--- app/src/main/java/edu/us/ischool/info448/songspot/activites/LoginPage.kt
+++ app/src/main/java/edu/us/ischool/info448/songspot/activites/LoginPage.kt
@@ -3,25 +3,34 @@ package edu.us.ischool.info448.songspot.activites
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+//import com.google.firebase.auth.FirebaseAuth
+//import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
 import edu.us.ischool.info448.songspot.R
 
 class LoginPage : AppCompatActivity() {
 
     private lateinit var database: DatabaseReference
+    private var loginButton : Button = findViewById(R.id.loginButton)
+    private var username : EditText = findViewById(R.id.username)
+    private var password : EditText = findViewById(R.id.password)
+    private var registerButton : Button = findViewById(R.id.register)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
 
         database = FirebaseDatabase.getInstance().reference
-
-        var loginButton : Button = findViewById(R.id.loginButton)
-        var username : EditText = findViewById(R.id.username)
-        var password : EditText = findViewById(R.id.password)
-        var registerButton : Button = findViewById(R.id.register)
+//        auth = FirebaseAuth.getInstance()
+//
+//        var loginButton : Button = findViewById(R.id.loginButton)
+//        var username : EditText = findViewById(R.id.username)
+//        var password : EditText = findViewById(R.id.password)
+//        var registerButton : Button = findViewById(R.id.register)
 
         loginButton.setOnClickListener {
             loginUser(username.text.toString(), password.text.toString())
@@ -33,25 +42,56 @@ class LoginPage : AppCompatActivity() {
         }
     }
 
-    //  Logs user into their account after taking in a username and password.
-    // Will check database if username/password combo exists
+
+//    private fun signIn(email: String, password: String) {
+//        // [START sign_in_with_email]
+//        auth.signInWithEmailAndPassword(email, password)
+//            .addOnCompleteListener(this) { task ->
+//                if (task.isSuccessful) {
+//                    // Sign in success, update UI with the signed-in user's information
+//                    Log.w("TAG", "signInWithEmail:success")
+//                    // Take to new activity
+//                } else {
+//                    // If sign in fails, display a message to the user.
+//                    Log.w("TAG", "signInWithEmail:failure", task.exception)
+//                }
+//            }
+//    }
+
+//      Logs user into their account after taking in a username and password.
+//     Will check database if username/password combo exists
     private fun loginUser(username: String, password:String) {
-        // check if user exists
-        // if user exists take them to the application
-        // if they do not, don't do anything
-        database.child("users").addListenerForSingleValueEvent(object: ValueEventListener{
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                if (dataSnapshot.child(username).exists() &&
-                    dataSnapshot.child(username).child("password").getValue(true) == password) {
-                    println("USER LOGIN AUTHENICATED")
-                    /**
-                     *  ADD IN USERNAME AS ARG TO GIVE TO GENRE PICKING PAGE
-                     *  THEN START GENRE PAGE ACTIVITY
-                     */
+        if (validForm()) {
+            database.child("users").addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    if (dataSnapshot.child(username).exists() && dataSnapshot.child(username).child("password").getValue(
+                            true
+                        ) == password
+                    ) {
+                        println("USER LOGIN AUTHENICATED")
+                        /**
+                         *  ADD IN USERNAME AS ARG TO GIVE TO GENRE PICKING PAGE
+                         *  THEN START GENRE PAGE ACTIVITY
+                         */
+                    }
                 }
-            }
-            override fun onCancelled(p0: DatabaseError) {
-            }
-        })
+
+                override fun onCancelled(p0: DatabaseError) {
+                }
+            })
+        }
+    }
+
+    // Checks whether form is properly filled
+    private fun validForm() : Boolean {
+        if (username.text.isEmpty()) {
+            // do something
+            return false
+        }
+        if (password.text.isEmpty()) {
+            // do something
+            return false
+        }
+        return true
     }
 }
--- app/src/main/java/edu/us/ischool/info448/songspot/activites/RegisterPage.kt
+++ app/src/main/java/edu/us/ischool/info448/songspot/activites/RegisterPage.kt
@@ -1,51 +1,107 @@
 package edu.us.ischool.info448.songspot.activites
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+//import com.google.firebase.auth.FirebaseUser
 //import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
 import edu.us.ischool.info448.songspot.R
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.database.*
+
 
 class RegisterPage : AppCompatActivity() {
 
     private lateinit var database: DatabaseReference
-    //private lateinit var auth: FirebaseAuth
+    private var registerButton : Button = findViewById(R.id.loginButton)
+    private var username : EditText = findViewById(R.id.username)
+    private var display : EditText = findViewById(R.id.displayName)
+    private var password : EditText = findViewById(R.id.password)
+//    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_page)
 
         database = FirebaseDatabase.getInstance().reference
-        //auth = FirebaseAuth.getInstance()
+//        auth = FirebaseAuth.getInstance()
 
-        var registerButton : Button = findViewById(R.id.loginButton)
-        var username : EditText = findViewById(R.id.username)
-        var display : EditText = findViewById(R.id.displayName)
-        var password : EditText = findViewById(R.id.password)
+//        var registerButton : Button = findViewById(R.id.loginButton)
+//        var username : EditText = findViewById(R.id.username)
+//        var display : EditText = findViewById(R.id.displayName)
+//        var password : EditText = findViewById(R.id.password)
 
         registerButton.setOnClickListener {
             createNewUser(username.text.toString(), display.text.toString(), password.text.toString())
         }
+
     }
 
-    // Creates a new account
+//    private fun createAccount(email: String, password: String) {
+//        // [START create_user_with_email]
+//        auth.createUserWithEmailAndPassword(email, password)
+//            .addOnCompleteListener(this
+//            ) { task ->
+//                if (task.isSuccessful) {
+//                    //User registered successfully
+//                    // Need to put display name in database
+//
+//
+//                // Will handle already existing emails
+//                } else {
+//                    Log.i("Response", "Failed to create user:" + task.exception!!.message)
+//                }
+//            }
+//    }
+
+    //  Creates a new account
     private fun createNewUser(username: String, name: String?, password: String) {
-        //database.child("users").child(username).setValue(password)
-        //val screenName = if (name.equals(null)) username else name
-        //database.child("users").child(username).setValue(screenName)
-        // check if selected username exists within database
-        // if it doesn't exist in database, create it username/displayname/password combo
-        // if it does, notify user that this username is already in use
-        val thisUser = User(username, name, password)
-        database.child("users").child(username).setValue(thisUser)
-        val intent = Intent(this, LoginPage::class.java)
-        startActivity(intent)
+        // Context
+        val thisContext = this
+
+        if (validForm()) {
+            database.child("users").addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    // Have to hash and turn into a string because Firebase can't handle certain special characters
+                    val hashedUser = username.hashCode().toString()
+                    // If email doesn't eixst, create account
+                    if (!dataSnapshot.child(hashedUser).exists()) {
+                        val thisUser = User(username, name, password)
+                        Log.i("TAG", hashedUser)
+                        database.child("users").child(hashedUser).setValue(thisUser)
+                        val intent = Intent(thisContext, LoginPage::class.java)
+                        startActivity(intent)
+                        // If email does exist in the database, tell user username already exists.
+                    } else {
+                        Toast.makeText(thisContext, "This username already exists!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                override fun onCancelled(p0: DatabaseError) {
+                }
+            })
+        }
     }
 
+    private fun validForm() : Boolean {
+        if (username.text.isEmpty()) {
+            // do something
+            return false
+        }
+        if (display.text.isEmpty()) {
+            // do something
+            return false
+        }
+        if (password.text.isEmpty()) {
+            // do something
+            return false
+        }
+        return true
+    }
 }
 
 // Dataclass representing user information. This will be used to update/retrieve data information
