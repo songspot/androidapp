55cdfb198cbac07dbd1de0ee0cf3e43a8ee63e14
Jonathan <jonathan.lin1138@live.com>
2019-06-08 00:31:03 -0700

    Added basic points system, Spotify support

--- app/src/main/java/edu/us/ischool/info448/songspot/activites/MainActivity.kt
+++ app/src/main/java/edu/us/ischool/info448/songspot/activites/MainActivity.kt
@@ -3,11 +3,13 @@ package edu.us.ischool.info448.songspot.activites
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.spotify.sdk.android.authentication.AuthenticationRequest
 import edu.us.ischool.info448.songspot.R
 import edu.us.ischool.info448.songspot.api.App
 import com.spotify.sdk.android.authentication.AuthenticationClient
 import com.spotify.sdk.android.authentication.AuthenticationResponse
+import edu.us.ischool.info448.songspot.QuestionActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,18 +28,20 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, LoginPage::class.java)
         startActivity(intent)
 
+
         val request = AuthenticationRequest.Builder(clientId, AuthenticationResponse.Type.TOKEN, redirectUri)
             .setScopes(arrayOf("user-read-private", "playlist-read", "playlist-read-private", "streaming"))
             .build()
 
         AuthenticationClient.openLoginActivity(this, requestCode, request)
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent) {
         super.onActivityResult(requestCode, resultCode, intent)
 
         // Check if result comes from the correct activity
-
+        Log.d("debugging", "Reached here!")
         val response = AuthenticationClient.getResponse(resultCode, intent)
         when (response.type) {
             // Success, response contains accessToken
@@ -62,5 +66,6 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
+
     }
 }
