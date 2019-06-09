3f69ed0956c9f14c63793f8c271a6008274adecb
matthewngo <mvn3@uw.edu>
2019-06-09 13:24:36 -0700

    new Leaderboard Page, fixed registering bug

---
+++ app/src/main/java/edu/us/ischool/info448/songspot/activites/LeaderboardPage.kt
@@ -0,0 +1,15 @@
+package edu.us.ischool.info448.songspot.activites
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import edu.us.ischool.info448.songspot.R
+
+class LeaderboardPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_leaderboard_page)
+
+
+    }
+}
